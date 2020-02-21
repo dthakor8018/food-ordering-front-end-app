09705f26b802b3eb2549c5466cc84ca683064a3e
dthakor <dthakor@vmware.com>
2020-02-21 19:03:44 +0530

    fixing header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,106 +1,118 @@
-import React, { Component } from 'react';
-import './Header.css';
-import Input from '@material-ui/core/Input';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
-import Avatar from "@material-ui/core/Avatar";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
-class Header extends Component {
-    constructor() {
-        super();
-        this.state = {
-            anchorEl: null,
-            searchValue: "",
-            userProfileData: null
-        };
-
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block"
     }
-
-    /* get User Profile data */
-    getUserProfile() {
-        fetch(this.props.baseUrl + "?access_token=" + sessionStorage.getItem("access-token"))
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({ userProfileData: result.data });
-
-                },
-                (error) => {
-                    console.log("error...", error);
-                }
-            );
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: "#263238",
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto"
     }
-
-    /*check user is logged in or not*/
-    componentDidMount() {
-        if (this.state.loggedIn === false) {
-         //   this.props.history.push("/");
-        }
-        this.getUserProfile();
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200
     }
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
+  }
+}));
 
-    menuOpenHandler = (event) => {
-        this.setState({ anchorEl: event.currentTarget });
-    };
-    menuCloseHandler = () => {
-        this.setState({ anchorEl: null });
-    };
-    /* remove the access token when user logout*/
-    logoutHandler = () => {
-        sessionStorage.removeItem("access-token");
-        this.menuCloseHandler();
-        this.props.history.push("/");
-    };
+export default function Header() {
+  const classes = useStyles();
 
-    /*redirect to profile page*/
-    profileRedirect = () => {
-        this.props.history.push("/profile");
-    };
-    /*redirect to home page*/
-    homeRedirect = () => {
-        this.props.history.push("/home");
-    };
-
-    render() {
-        return (
-            <div className="header">
-                <div className="title" onClick={this.homeRedirect}>Food Ordering App</div>
-
-                <div className="header-right">
-                    {this.props.showSearchBar === true ?
-                        <div id="search-field">
-                            <div className="searchIcon">
-                                <SearchIcon/>
-                            </div>
-                            <Input className="searchInput" onChange={this.props.searchChangeHandler} disableUnderline={true}
-                                   placeholder="Search..."/>
-                        </div> : ""}
-                    <IconButton id="profile-icon" edge="start" color="inherit" aria-label="menu">
-                        {this.state.userProfileData ?
-                            <Avatar alt={this.state.userProfileData.full_name} id="profile-icon" fontSize="small"
-                                    ariant="circle" src={this.state.userProfileData.profile_picture}
-                                    onClick={this.menuOpenHandler}/> : null}
-                        <Menu
-                            id="simple-menu"
-                            anchorEl={this.state.anchorEl}
-                            keepMounted
-                            open={Boolean(this.state.anchorEl)}
-                            onClose={this.menuCloseHandler}
-                        >
-                            <MenuItem onClick={this.profileRedirect}>My Account</MenuItem>
-                            <MenuItem onClick={this.logoutHandler}>Logout</MenuItem>
-                        </Menu>
-                    </IconButton>
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" style={{ backgroundColor: "#263238" }}>
+        <Toolbar>
+          <Grid container>
+            <Grid item xs={12} sm={3}>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="open drawer"
+              >
+                <FastfoodIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div
+                className={classes.search}
+                style={{ borderBottom: "solid black 0.1rem" }}
+              >
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
                 </div>
-
-            </div>
-
-        );
-    }
-
-
+                <InputBase
+                  placeholder="Search by Restaurant Name"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput
+                  }}
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={3} style={{ gridColumnStart: "revert" }}>
+              <div className={classes.grow} />
+              <div className={classes.sectionDesktop}>
+                <Button variant="contained">Login</Button>
+              </div>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-export default Header;
