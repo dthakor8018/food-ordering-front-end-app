3f5eb39567368e9867e565a65cf3f869b9466dd6
Dipesh Singhal <dipesh.singhal@gmail.com>
2020-02-24 21:19:43 +0530

    Fetching data for Home page i.e. All restaurant details

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -9,8 +9,48 @@ class Home extends Component {
     super();
     this.state = {
       loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
-      openLoginSignupModal: false
+      openLoginSignupModal: false,
+      restaurantData: null,
+      error: false,
+      erorCode: null,
+      errorMsg: null
     };
+  }
+  componentWillMount() {
+    fetch(
+      this.props.baseUrl + "restaurant",
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Accept': 'application/json;charset=UTF-8',
+          'Access-Control-Allow-Headers': true,
+        }
+      }
+    ).then((response) => {
+      if (response.status === 200) {
+        response.json().then((json) => {
+          console.log(json);
+          this.setState({ restaurantData: json });
+        })
+      } else {
+        console.log("login Error " + response.status);
+        response.json().then((json) => {
+          this.setState({
+            error: true,
+            erorCode: json.code,
+            errorMsg: json.message
+          });
+        })
+      }
+    }, error => {
+      console.log("Error while making request to FoodOrderingApp Backend", error)
+      this.setState({
+        error: true,
+        erorCode: error.code,
+        errorMsg: "Error while making request to FoodOrderingApp Backend"
+      });
+    })
   }
 
   openLoginSignupModal = () => {
@@ -22,22 +62,22 @@ class Home extends Component {
     this.setState({
       openLoginSignupModal: false,
       loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
-    })    
+    })
   }
   logoutHandler = () => {
     this.setState({
       openLoginSignupModal: false,
       loggedIn: sessionStorage.getItem("access-token") == null ? false : true,
-    }) 
+    })
   }
   render() {
     return (
       <div>
-        <Header {...this.props} showSearchBar={true} loggedIn={this.state.loggedIn} logoutHandler={this.logoutHandler} openLoginSignupModal={this.openLoginSignupModal}/>
-        <RestaurantCard/>
+        <Header {...this.props} showSearchBar={true} loggedIn={this.state.loggedIn} logoutHandler={this.logoutHandler} openLoginSignupModal={this.openLoginSignupModal} />
+        <RestaurantCard />
         <LoginSignupModal {...this.props}
-                openLoginSignupModal={this.state.openLoginSignupModal}
-                onCloseLoginSignupModal={this.onCloseLoginSignupModal}/>
+          openLoginSignupModal={this.state.openLoginSignupModal}
+          onCloseLoginSignupModal={this.onCloseLoginSignupModal} />
       </div>
     );
   }
