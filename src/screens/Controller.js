93b00ac68f11a31a75f08af9de4dcfc8056997a5
dthakor <dthakor@vmware.com>
2020-02-28 15:24:36 +0530

    checkout page WIP_1

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './home/Home';
 import Profile from './profile/Profile';
 import Details from './details/Details';
+import Checkout from "./checkout/Checkout";
 
 
 class Controller extends Component {
@@ -16,6 +17,7 @@ class Controller extends Component {
           <div className="main-container">
             <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
             <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
+              <Route exact path='/checkout' render={(props) => <Checkout {...props} baseUrl={this.baseUrl} />} />
             <Route path='/restaurant/:restaurantId' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
           </div>
         </Router>
