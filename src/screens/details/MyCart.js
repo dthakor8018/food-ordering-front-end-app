699b0b79112d811fc9ec7f5ffaaee4c776f2e6d1
Dipesh Singhal <dipesh.singhal@gmail.com>
2020-02-29 17:39:30 +0530

    Modify MyCart

--- src/screens/details/MyCart.js
+++ src/screens/details/MyCart.js
@@ -6,8 +6,11 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import { List, ListItem, ListItemText, ListItemAvatar, Badge } from '@material-ui/core';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRupeeSign, faCircle, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+// import { faRupeeSign, faCircle, faPlus, faStar } from '@fortawesome/free-solid-svg-icons'
 
 
 const useStyles = makeStyles({
@@ -31,22 +34,41 @@ export default function MyCart() {
   const classes = useStyles();
   return (
     <div>
-    <Card className={classes.root} variant="outlined">
-      <CardContent>
-        <ShoppingCartIcon />
-        <Typography className={classes.title} color="textPrimary" gutterBottom>
-          My Cart
-        </Typography>
-        <div id="cart">
-        </div>
-        <Typography className={classes.pos} color="textSecondary">
-          TOTAL AMOUNT
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" color="primary" >CHECKOUT</Button>
-      </CardActions>
-    </Card>
+      <Card className={classes.root} variant="outlined">
+        <CardContent>
+        <Badge badgeContent={4} color="primary">
+          <ShoppingCartIcon />
+        </Badge>
+          <Typography variant="h5" component="div">
+            My Cart
+          </Typography>
+          <List>
+            <ListItem>
+              <ListItemAvatar>
+                <FontAwesomeIcon icon={faCircle} color="green" />
+              </ListItemAvatar>
+              <ListItemText style={{ width: '30%' }} color="textSecondary"  >{"Test Item"}</ListItemText>
+              <ListItemText>
+                <Typography variant="body1" color="textPrimary"><FontAwesomeIcon icon={faMinus} />{" "+1+" "}<FontAwesomeIcon icon={faPlus} /></Typography>
+              </ListItemText>
+              <ListItemText>
+                <Typography variant="body1" color="textSecondary"><FontAwesomeIcon icon={faRupeeSign} />140</Typography>
+              </ListItemText>
+            </ListItem>
+            <ListItem>
+            <ListItemText style={{ width: '60%' }}>
+                <Typography variant="body1" color="textPrimary">TOTAL AMOUNT</Typography>
+              </ListItemText>
+              <ListItemText>
+                <Typography variant="body1" color="textPrimary"><FontAwesomeIcon icon={faRupeeSign} />140</Typography>
+              </ListItemText>
+            </ListItem>
+          </List>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" fullWidth={true} color="primary">CHECKOUT</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
